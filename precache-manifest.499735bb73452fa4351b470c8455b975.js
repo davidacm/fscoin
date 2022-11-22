737084c086809aa933262ef64cd4b2ebe25f0e8c
davidacm <dhf360@gmail.com>
2022-11-22 14:27:14 -0600

    Update

---
+++ precache-manifest.499735bb73452fa4351b470c8455b975.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf120ef61a1434ac1958",
+    "url": "/fscoin/css/app.d40fc157.css"
+  },
+  {
+    "revision": "21c22f71928f161610ba",
+    "url": "/fscoin/css/chunk-vendors.8d640e24.css"
+  },
+  {
+    "revision": "e503bb651d2ef480ab4fd589f4ec1d38",
+    "url": "/fscoin/img/logo.e503bb65.png"
+  },
+  {
+    "revision": "1653bdc5f62bcb9c0607703757b19f92",
+    "url": "/fscoin/index.html"
+  },
+  {
+    "revision": "bf120ef61a1434ac1958",
+    "url": "/fscoin/js/app.1ff188ab.js"
+  },
+  {
+    "revision": "21c22f71928f161610ba",
+    "url": "/fscoin/js/chunk-vendors.71bbf8c7.js"
+  },
+  {
+    "revision": "f5b62e05eead0ba705389654ce98cb93",
+    "url": "/fscoin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fscoin/robots.txt"
+  }
+]);
